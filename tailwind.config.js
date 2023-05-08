dd03715b0b44a65197f1da1c9851cafd4f95a0b2
hqunted <berkekirci@gmail.com>
2023-05-08 15:43:47 +0200

    feat: add animation to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 2s ease-in forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      variants: {
+        animation: ["motion-safe"],
+      },
+    },
   },
   plugins: [],
 };
